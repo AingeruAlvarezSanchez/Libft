613c170120893f52b3349c0b51951574c62824af
benatcastro <benatcastro@gmail.com>
2022-09-20 08:30:42 +0200

    ft_strcmp

---
+++ ft_strcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_utils_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 20:22:28 by becastro          #+#    #+#             */
+/*   Updated: 2022/09/19 19:10:43 by becastro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @brief
+ * Str compare refactorized
+ * @param s1
+ * @param s2
+ * @return true
+ * @return false
+ */
+int	ft_strcmp(char *s1, char *s2)
+{
+	int32_t	i;
+
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (0);
+	i = -1;
+	while (s1[++i])
+		if (s1[i] != s2[i])
+			return (0);
+	return (1);
+}
