e28d09b5d68a46cf27d53862a76c09090ff34270
becastro <benatcastro@gmail.com>
2022-09-24 12:56:19 +0200

    ft_isempty refactor, now checks for more blanc chars than spaces

--- ft_isempty.c
+++ ft_isempty.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isempty.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 21:02:27 by aalvarez          #+#    #+#             */
-/*   Updated: 2022/08/24 00:29:18 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/09/24 12:55:03 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 /**
  * @brief checks if the string pointed by str has at least onw character
  * that differs from a space bar or its significants.
- * 
+ *
  * @param str the string to be evaluated.
  * @return int 1 if it finds a character differing from printable char, 0
  * otherwise.
  */
-int	ft_isempty(const char *str)
+int	ft_strnotblank(const char *str)
 {
 	if (!str)
 		return (0);
 	while (*str)
 	{
-		if (ft_isprint(*str) && *str != ' ')
+		if (ft_isprint(*str) && !ft_isblank(*str))
 			return (1);
 		str++;
 	}
