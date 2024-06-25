1ac684999704c220358aa72b90c76837753e4cb2
aalvarez <aingeru.develop@gmail.com>
2024-06-26 00:41:57 +0930

    Feature ✨: is_something family functions added.
    Signed-off-by: aalvarez <aingeru.develop@gmail.com>

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalvarez <aalvarez@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 12:54:27 by aalvarez          #+#    #+#             */
+/*   Updated: 2024/06/26 00:18:23 by aalvarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+// is_something family functions.
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+#endif //LIBFT_H
